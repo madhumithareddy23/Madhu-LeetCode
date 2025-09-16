a8b617066108d13364f961d541a8bb4206f23b8b
Madhumitha Reddy <155007336+madhumithareddy23@users.noreply.github.com>
2025-09-16 19:24:42 +0530

    Time: 35 ms (48.45%) | Memory: 58.3 MB (92.78%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        List<Integer> stack = new ArrayList<>();
+        
+        for (int num : nums) {
+            stack.add(num);
+            
+            // Keep merging as long as last two are non-coprime
+            while (stack.size() >= 2) {
+                int a = stack.get(stack.size() - 2);
+                int b = stack.get(stack.size() - 1);
+                int g = gcd(a, b);
+                if (g == 1) break; // coprime, stop merging
+                
+                // Merge into LCM
+                long lcm = (long) a / g * b;
+                stack.remove(stack.size() - 1);
+                stack.remove(stack.size() - 1);
+                stack.add((int) lcm);
+            }
+        }
+        
+        return stack;
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
